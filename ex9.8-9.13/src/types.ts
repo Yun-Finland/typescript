3568c2a4f9414b6c18b79c295ff1c74aa87c2a9f
Yun Xiao <yun.xiao.finland@gmail.com>
2021-09-30 20:29:29 +0300

    ex9.19

--- ex9.8-9.13/src/types.ts
+++ ex9.8-9.13/src/types.ts
@@ -10,9 +10,44 @@ export enum Gender {
   Other = "other",
 }
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
 }
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>;
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: {
+    date: string;
+    criteria: string;
+  };
+}
+
+export interface OccupationalHealthCareEntry  extends BaseEntry {
+  type: 'OccupationalHealthcare';
+  employerName: string;
+  sickLeave?:{
+    startDate: string;
+    endDate: string;
+  };
+}
+
+export type Entry = OccupationalHealthCareEntry | HospitalEntry | HealthCheckEntry;
 
 export interface Patient {
   id: string;
